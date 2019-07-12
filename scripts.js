d2908cb1414ebb88e2b8d11ab5055cb945d76376
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-07-12 10:11:35 -0500

    Removed for loop in JS and used forEach() instead

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 // Select first article_header and change textContent to "Welcome to the Sarah Fleming blog"
-document.querySelector(".article__header").textContent = "Welcome to the Sarah Fleming blog";
+document.querySelector(".article__header").textContent = "Welcome to the Sarah Fleming blog"
 
 //Create variable allHeaders that selects all elements with class "article__header"
-const allHeaders = document.querySelectorAll(".article__header");
+const allHeaders = document.querySelectorAll(".article__header")
 
-//Loop through the values stored in the allHeaders array and add the class "important"
-for ( i = 0; i < allHeaders.length; i++ ) {
-    allHeaders[i].classList.add("important");
-}
+//Add new clas named "important" to all elements with class "article__header"
+allHeaders.forEach(
+    (currentNode) => currentNode.classList.add("important")
+)
 
 //Select element with class "dashed" and remove the class
-document.querySelector(".dashed").classList.remove("dashed");
+document.querySelector(".dashed").classList.remove("dashed")
 
 //Select element with class "article__footer" and add class "goldenrod"
-document.querySelector(".article__footer").classList.add("goldenrod");
+document.querySelector(".article__footer").classList.add("goldenrod")
